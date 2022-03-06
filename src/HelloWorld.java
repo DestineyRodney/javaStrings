53336e359bb818bea6393ebdc6f2c9f04df90d4d
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-05 20:47:16 -0600

    feat: intro to java

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        int myFavNum = 5;
+        String myString = "Hello honey";
+//        long myNumber = 123;
+         double myFloat = 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        int three = (int) 3.14;
+        short num = 32767;
+        num ++;
+        System.out.println(num);
+    }
+}
