a9ba8e0ba46eeedf24fab2f88519707fbb55d400
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-06 23:22:48 -0600

    feat: using loops

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -47,12 +47,53 @@ public class HelloWorld {
 //        int area = (userLength * userWidth);
 //        System.out.println("Area: " + area);
 //        Using DELIMITER
-        scanner.useDelimiter("\n*");
+//        scanner.useDelimiter("\n");
+//
+//        System.out.println("Enter length and width");
+//        System.out.println(scanner.nextInt());
+//        System.out.println(scanner.nextInt());
+//        scanner.close();
 
-        System.out.println("Enter length and width");
-        System.out.println(scanner.nextInt());
-        System.out.println(scanner.nextInt());
-        scanner.close();
+        int caseSwitch = 1;
+
+        switch (caseSwitch) {
+            case 1:
+                System.out.println("Case 1");
+                // Fall through!
+            case 2:
+                System.out.println("Case 2");
+                break;
+            default:
+                System.out.println("Default case");
+                break;
+        }
+
+//        int i = 5;
+//        while (i < 15){
+//            ++i;
+//            System.out.println(i);
+//        }
+
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//         int i = 0;
+//        do {
+//            System.out.println(i += 2);
+//        }while (i < 100);
+//Alter your loop to count backwards by 5's from 100 to -10.
+
+//        int i = 100;
+//        do {
+//            System.out.println(i -= 5);
+//        }while (i > -10);
+
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        long i = 2;
+
+        do {
+            System.out.println(i);
+
+            i= i * i;
+        }while (i< 1000000);
 
 
 
