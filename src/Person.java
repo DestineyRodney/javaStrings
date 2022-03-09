b0b4004662bc09f43a330a47bd93637848cc8973
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-08 23:23:39 -0600

    feat: OOP

---
+++ src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private String name;
+
+  public Person (String name){
+      this.name = name;
+  }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        System.out.println(this.name);
+    }
+
+    public void sayHello(){
+        System.out.printf("Hello %s ", this.name);
+    }
+
+    public static void main (String[] args){
+      Person person1 = new Person("Bob");
+        System.out.println(person1.getName());
+
+        person1.setName("Niya");
+    }
+}
