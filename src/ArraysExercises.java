967d010e0a15697c6e57c141278b10cc6cefb433
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-10 23:30:00 -0600

    feat: arrays

---
+++ src/ArraysExercises.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main (String[] args){
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+    Person[] students = new Person[3];
+    students[0] = new Person("Niya");
+    students[1] = new Person("Tk");
+    students[2] = new Person("Des");
+    }
+}
