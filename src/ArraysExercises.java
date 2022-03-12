b68e1390acb1a28c7c8dfefcfb4f61c362f600a2
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-11 20:44:32 -0600

    feat: generator

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,12 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+
+    public static Person[] addPerson (Person[] array, Person single){
+        Person[] addOnePerson = Arrays.copyOf(array, array.length + 1 );
+                addOnePerson[addOnePerson.length - 1] = new Person(single.getName());
+                return addOnePerson;
+    }
     public static void main (String[] args){
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,18 @@
+public class ServerNameGenerator {
+    static String[] adjectives = {"charming", "Cruel", "Fantastic", "Gentle", "Huge", "Perfect", "Rough", "Sharp", "Smooth", "Tasty"};
+
+    static String[] nouns = {"man", "woman", "teacher", "doctor", "beans", "dog", "monkey", "music", "home", "school"};
+
+  public static String getValue(String[] arr){
+      int random = (int) (Math.random() * arr.length -1);
+      return arr[random];
+  }
+
+    public static void main(String[] args) {
+
+        String adj = getValue(adjectives);
+        String noun = getValue(nouns);
+        System.out.println("Here is your server name:");
+        System.out.println(adj + "-" + noun);
+    }
+}
