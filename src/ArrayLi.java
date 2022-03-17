a440948a2756b82a9e979a4fda106528e287f290
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-16 20:32:50 -0500

    feat: Students ArrayList

--- src/ArrayLi.java
+++ src/ArrayLi.java
@@ -1,10 +1,33 @@
 import java.util.ArrayList;
 public class ArrayLi {
-    private String name;
-    private ArrayList<String> cars;
+//    private String name;
+//    private ArrayList<String> cars;
+//
+//    public ArrayLi(String name) {
+//        this.name = name;
+//        this.cars = cars = new ArrayList<>();
+//    }
+    public static void main (String[] args){
+        ArrayList<String> cars = new ArrayList<String>();
 
-    public ArrayLi(String name) {
-        this.name = name;
-        this.cars = cars = new ArrayList<>()
+        cars.add("a");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+
+        System.out.println(numbers);
+        System.out.println(numbers.get(0));
+
+        ArrayList<Double> num = new ArrayList<>();
+        num.add(5.6);
+        num.add(7.2);
+        System.out.println(num);
+        num.add(1, 7.6 );
+        System.out.println(num);
+        System.out.println(num.contains(7.2));
+        System.out.println(num.isEmpty());
+        num.remove(1);
+        System.out.println(num);
+
+
     }
 }
---
+++ src/grades/Student.java
@@ -0,0 +1,38 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String studentName;
+    private ArrayList<Integer> grades;
+
+    public Student(String studentName){
+        this.studentName = studentName;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getStudentName(){
+        return this.studentName;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        int sum = 0;
+      for(int i = 0; i < grades.size(); i++){
+          sum = sum + grades.get((int) i);
+      }
+        return sum / grades.size();
+    }
+
+    public static void main (String[] args){
+        Student student1 = new Student("Ryan");
+        student1.addGrade(97);
+        student1.addGrade(100);
+        student1.addGrade(50);
+
+        System.out.println(student1.getGradeAverage());
+    }
+}
